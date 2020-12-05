428d6a725f4318a7c2fa8596845678f99a471ad8
Pratikshya Regmi <61108147+Prati-hub@users.noreply.github.com>
2020-12-05 13:37:34 +0545

    Add files via upload

---
+++ ezosuigeneris.js
@@ -0,0 +1,28 @@
+var ezosuigeneris = '0c28aa73403946a8808fdd97d4cbdfaf';
+if(typeof ezosuigeneris != "undefined")
+{
+    var myDate = new Date();
+    myDate.setMonth(myDate.getMonth() + 24);
+    document.cookie = "ezosuigeneris=" + ezosuigeneris + ";expires=" + myDate + ";domain="+ezdomain+";path=/";
+}
+
+function callezosuigeneris() {
+	var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "//g.ezoic.net/ezosuigeneris.js";
+    document.body.appendChild(s);
+}
+if(window.attachEvent) {
+	window.attachEvent('onload', ezosuigeneris);
+} else {
+    if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function(evt) {
+            curronload(evt);
+            callezosuigeneris();
+        };
+        window.onload = newonload;
+    } else {
+        window.onload = ezosuigeneris;
+    }
+}
